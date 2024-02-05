04ce52a4cbba13dfa4edad3807c5bdcef8343049
Kenneth Shum <158795476+wkshum@users.noreply.github.com>
2024-02-05 13:44:44 +0800

    Added comments and explanation

--- AEP.py
+++ AEP.py
@@ -1,7 +1,26 @@
 """
+Asymptotic Equipartition Property
+
 Generate a random string with each letter generated independently according to 
 a given distribution, and calculate the log of the probability that this particular
 string is drawn.
+
+
+A typical run of this program produces the following output:
+
+acabcbabdabcaadaaaaaaababaabbaacaaaaaadaaccacababaababaababbbabbaababbcaadbaaabcbaaabbbacbaaaaadabab
+a occurs 55 times
+b occurs 30 times
+c occurs 10 times
+d occurs 5 times
+log_2(probability) =  -160.0
+
+Among the 100 random characters, 55 of them are 'a', 30 of them are 'b', 10 of them are 'c' and 5 of them are 'd'.
+This is in accordance with the distribution [1/2, 1/4, 1/8, 1/8].
+The log of the probability of this string (in base 2) is -160.
+
+According to the weak law of large number, the log of probability is approximately equal to the entropy of the
+distribution times the string length.
 """
 
 
